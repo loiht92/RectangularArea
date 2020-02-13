a1f20ec5c2ecf22048bc77016844c05b260d392d
loiminh <loiho11082015@gmail.com>
2020-02-13 11:44:51 +0700

    add file

---
+++ src/RectangularArea.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RectangularArea {
+    public static void main(String[] args) {
+        float width ;
+        float height ;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a width");
+        width = scanner .nextFloat();
+        System.out.println("Enter a height");
+        height = scanner.nextFloat();
+
+        float area = width * height ;
+        System.out.println(" Area is" +area);
+
+    }
+}
